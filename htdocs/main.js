2e5122e69a36f00ddff368b030e4f9255f2d0210
Michael Mouton <michael.mouton510@gmail.com>
2025-04-22 20:29:08 +0200

    Update main.js

--- htdocs/main.js
+++ htdocs/main.js
@@ -1,17 +1,14 @@
 const output = document.getElementById('output');
-const interval = setInterval(() => {
-  fetch('/cgi-bin/index.html')
-    .then(response => {
-      if (!response.ok) throw new Error('Network response was not ok');
-      return response.text();
-    })
-    .then(text => {
-      if (text.trim() !== "") {
-        output.innerText = text;
-        clearInterval(interval);
-      }
-    })
-    .catch(error => {
-      console.error('Error fetching content:', error);
-    });
+const interval = setInterval(async () => {
+  try {
+    const response = await fetch('/cgi-bin/index.html');
+    if (!response.ok) throw new Error('Network response was not ok');
+    const text = await response.text();
+    if (text.trim() !== "") {
+      output.innerText = text;
+      clearInterval(interval);
+    }
+  } catch (error) {
+    console.error('Error fetching content:', error);
+  }
 }, 250);
